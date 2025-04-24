edfc41a911ec5e72d884f406bec527888956b2a2
Chris Galvan <chgalvan@amazon.com>
2025-04-24 13:15:03 -0500

    Fixed compile error in test

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,6 +12,7 @@ module.exports = {
       rules: {
         "@typescript-eslint/no-non-null-assertion": "off",
         "@typescript-eslint/no-empty-function": "off",
+        "@typescript-eslint/ban-ts-comment": "off",
       },
     },
   ],
